5ccfd2ab63c1341bea4b704309ae4c73baf7d84e
Ashish Mishra <anshm9448@gmail.com>
2022-05-08 21:14:46 +0530

    3rd

---
+++ arrays/Majorityelement.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size(), candidate, counter;
+        srand(unsigned(time(NULL)));
+        while (true) {
+            candidate = nums[rand()%n ], counter = 0;
+            for (int num : nums) {
+                if (num == candidate) {
+                    counter++;
+                }
+            }
+            if (counter > n / 2) {
+                break;
+            }
+        }
+        return candidate;
+    }
+};
