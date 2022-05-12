3946c6dd4b626a8f6e39ba3524695f73b85b6573
Ashish Mishra <anshm9448@gmail.com>
2022-05-12 19:33:13 +0530

    solved some questions

---
+++ Strings/reversestringword.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string reverseWords(string s) {
+        string result;
+    int i = 0;
+    int n = s.length();
+
+    while(i < n){
+        while(i < n && s[i] == ' ') i++;
+        if(i >= n) break;
+        int j = i + 1;
+        while(j < n && s[j] != ' ') j++;
+        string sub = s.substr(i, j-i);
+        if(result.length() == 0) result = sub;
+        else result = sub + " " + result;
+        i = j+1;
+    }
+    return result;
+    }
+};
+
+/*
+lass Solution {
+public:
+
+    // function to reverse any part of string from i to j (just one word or entire string)
+    void reverseword(string &s, int i, int j){
+        while(i<j){
+          char t=s[i];
+          s[i++]=s[j];
+          s[j--]=t;
+        } 
+    }
+    
+    void reverseWords(string &s) {
+        
+        int i=0, j=0;
+        int l=0;
+        int len=s.length();
+        int wordcount=0;
+        
+        while(true){
+            while(i<len && s[i] == ' ') i++;  // skip spaces in front of the word
+            if(i==len) break;
+            if(wordcount) s[j++]=' ';
+            l=j;
+            while(i<len && s[i] != ' ') {s[j]=s[i]; j++; i++;} 
+            reverseword(s,l,j-1);                // reverse word in place
+            wordcount++;
+            
+        }
+        
+        s.resize(j);                           // resize result string
+        reverseword(s,0,j-1);                  // reverse whole string
+    }
+};
+*/
