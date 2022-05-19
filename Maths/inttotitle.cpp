704f591799084f41730e71a2fcd61b0cc9339a96
Ashish Mishra <anshm9448@gmail.com>
2022-05-19 17:45:46 +0530

    solved

---
+++ Maths/inttotitle.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string convertToTitle(int n)
+    {
+
+        string res;
+
+        char tmp;
+
+        while (n)
+        {
+
+            n -= 1;
+
+            tmp = 'A' + n % 26;
+
+            res = tmp + res;
+
+            n /= 26;
+        }
+
+        return res;
+    }
+};
