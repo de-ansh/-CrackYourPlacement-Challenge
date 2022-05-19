effef1a235135f11f6fe0daef47924a06e5fbae2
Ashish Mishra <anshm9448@gmail.com>
2022-05-19 17:44:20 +0530

    solved

---
+++ Maths/happyNumber.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int next(int n)
+    {
+        int sum = 0;
+        
+        while(n != 0)
+        {
+            sum += pow(n % 10,2);
+            n = n / 10;
+        }
+        
+        return sum;
+    }
+
+public:
+    bool isHappy(int n) {
+        int slow = next(n);
+        int fast = next(next(n));
+        
+        while(slow != fast)
+        {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        
+        return fast == 1 ;
+    }
+};
