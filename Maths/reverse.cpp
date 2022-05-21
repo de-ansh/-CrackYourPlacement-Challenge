b83bd7afe4eb7362d11f4c2cb3afa10132e5eaba
Ashish Mishra <anshm9448@gmail.com>
2022-05-21 23:32:57 +0530

    solved

---
+++ Maths/reverse.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int ans = 0;
+        while (x)
+        {
+            int temp = ans * 10 + x % 10;
+            if (temp / 10 != ans)
+                return 0;
+            ans = temp;
+            x /= 10;
+        }
+        return ans;
+    }
+};
