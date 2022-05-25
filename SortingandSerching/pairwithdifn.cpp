dda455a130c7970da8a8fba78b01757497db3c3a
Ashish Mishra <anshm9448@gmail.com>
2022-05-25 22:37:59 +0530

    solved one

--- SortingandSerching/pairwithdifn.cpp
+++ SortingandSerching/pairwithdifn.cpp
@@ -1,24 +1,31 @@
-// C++ program to find a pair with the given difference
 #include <bits/stdc++.h>
 using namespace std;
 
-// The function assumes that the array is sorted
+
 bool findPair(int arr[], int size, int n)
 {
-	int l=0,r=size-1;
-	while(l<size && r<size){
-		if(arr[r]-arr[l]==n){
-			cout<<arr[l]<<","<<arr[r];
+	int i = 0;
+	int j = 1;
+
+	
+	while (i < size && j < size)
+	{
+		if (i != j && (arr[j] - arr[i] == n || arr[i] - arr[j] == n) )
+		{
+			cout << "Pair Found: (" << arr[i] <<
+						", " << arr[j] << ")";
 			return true;
 		}
-		i
+		else if (arr[j]-arr[i] < n)
+			j++;
+		else
+			i++;
 	}
 
 	cout << "No such pair";
 	return false;
 }
 
-// Driver program to test above function
 int main()
 {
 	int arr[] = {1, 8, 30, 40, 100};
