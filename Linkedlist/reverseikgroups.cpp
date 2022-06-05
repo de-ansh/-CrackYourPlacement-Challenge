dcce8a3c89b1cc97e5b47c07c257a2c346cb57bc
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-05 22:28:39 +0530

    solved

---
+++ Linkedlist/reverseikgroups.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void reverse(ListNode *s, ListNode *e){
+        ListNode*p= NULL, *c=s, *n=s->next;
+        while(p!=e){
+            c->next=p;
+            p=c;c=n;
+            if(n!=NULL) n=n->next;
+        }
+    }
+    
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL||k==1) return head;
+        ListNode *dummy= new ListNode(-1);
+        dummy->next=head;
+        ListNode *beforeStart=dummy, *e= head;
+        int i=0;
+        while(e!=NULL){
+            i++;
+            if(i%k==0)
+            {
+                ListNode *s=beforeStart->next, *temp=e->next;
+                reverse(s,e);
+                beforeStart->next=e;
+                s->next=temp;
+                beforeStart=s;
+                e=temp;
+            }
+            else
+            {
+                e=e->next;
+            }
+        }
+        return dummy->next;
+        
+    }
+};
